98b32d72e8d85bd16f6debfb7d682750d6732a21
muhammad-jiku <muhammadjiku364@gmail.com>
2024-12-08 13:14:03 +0600

    authentication added

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,11 +1,23 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { UserButton, useUser } from '@clerk/nextjs';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Header() {
+  const { isSignedIn } = useUser();
+
   return (
     <div className='p-5 flex justify-between items-center border shadow-sm'>
       <Image src={'./logo.svg'} alt='logo' width={190} height={100} />
-      <Button>Get Started</Button>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <Link href={'/sign-in'}>
+          <Button>Get Started</Button>
+        </Link>
+      )}
     </div>
   );
 }
